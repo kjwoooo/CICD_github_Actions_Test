b407c50a5d4a5d68fd613c475ac2aebef8afa700
[Cloud3] 권재우 <kjw1111k@naver.com>
2024-10-30 15:31:13 +0900

    태스트용 코드 작성

--- src/main/java/com/example/cicd_test1/CicdTest1Application.java
+++ src/main/java/com/example/cicd_test1/CicdTest1Application.java
@@ -1,13 +1,22 @@
 package com.example.cicd_test1;
 
+import lombok.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CicdTest1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdTest1Application.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "hello, World";
 	}
 
 }
