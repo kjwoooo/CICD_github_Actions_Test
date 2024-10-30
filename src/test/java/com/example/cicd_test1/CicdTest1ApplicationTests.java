89e31ccdb68617c4aae64f153d517465d333069f
[Cloud3] 권재우 <kjw1111k@naver.com>
2024-10-30 14:14:16 +0900

    Initial commit

---
+++ src/test/java/com/example/cicd_test1/CicdTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd_test1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
