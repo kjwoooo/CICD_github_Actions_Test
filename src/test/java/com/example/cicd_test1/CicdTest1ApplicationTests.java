37df69427c975a23f2c22f38c186e3fbaa20d57d
[Cloud3] 권재우 <kjw1111k@naver.com>
2024-11-07 21:15:46 +0900

    ci수정 테스트 실패

--- src/test/java/com/example/cicd_test1/CicdTest1ApplicationTests.java
+++ src/test/java/com/example/cicd_test1/CicdTest1ApplicationTests.java
@@ -11,21 +11,20 @@ class CicdTest1ApplicationTests {
 //	@Test
 //	void contextLoads() {
 //	}
-
+//
+//
+//	@Test
+//	void alwaysPassTest() {
+//		// 항상 true 조건을 검사하므로 무조건 통과하는 테스트
+//		assertTrue(true);
+//	}
+//
 
 	@Test
-	void alwaysPassTest() {
-		// 항상 true 조건을 검사하므로 무조건 통과하는 테스트
-		//
-		assertTrue(true);
+	void alwaysFailTest() {
+		// 항상 false 조건을 검사하므로 무조건 실패하는 테스트
+		assertTrue(false);
 	}
-
-
-//	@Test
-//	void alwaysFailTest() {
-//		// 항상 false 조건을 검사하므로 무조건 실패하는 테스트
-//		assertTrue(false);
-//	}
 
 
 
